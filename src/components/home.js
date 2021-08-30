91486acf8ae93503f9ca47bdce41b4852d5cf7e9
yavnikaa <yavnikagarg3435@gmail.com>
2021-08-30 20:44:35 +0530

    Complete homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import '../index.css';
 import SubHeader from './sub-header';
+import yav_blue from '../assets/yav_blue.png'
+import yav_yellow from '../assets/yav_yellow.png'
+import yav_pink from '../assets/yav_pink.png'
 
 class Home extends Component{
     render(){ 
@@ -11,6 +14,42 @@ class Home extends Component{
         return (
             <div>
                 <SubHeader headerData={header}/>
+                <div className='strip'>
+                <div className='blue-box'>
+                    <img src={yav_blue} className='img-box'/>
+                </div>
+                <div className='strip-text'>
+                <div className='box-heading'> WEB DEVELOPMENT </div>
+                <div className='box-text'> As you explore other people's React apps, keep an eye out for common JavaScript patterns. 
+                You should start to recognize some of the concepts you've learned. </div>
+                <a href='./webd' className='box-button'> VIEW ALL </a>
+                </div>
+                </div>
+
+                <div className='strip-two'>
+                <div className='blue-box'>
+                    <img src={yav_yellow} className='img-box-two'/>
+                </div>
+                <div className='strip-text-two'>
+                <div className='box-heading'> WEBSITE DESIGN </div>
+                <div className='box-text'> As you explore other people's React apps, keep an eye out for common JavaScript patterns. 
+                You should start to recognize some of the concepts you've learned. </div>
+                <a href='./webd' className='box-button'> VIEW ALL </a>
+                </div>
+                </div>
+
+                <div className='strip-three'>
+                <div className='blue-box'>
+                    <img src={yav_pink} className='img-box-three'/>
+                </div>
+                <div className='strip-text-three'>
+                <div className='box-heading'> UX DESIGN </div>
+                <div className='box-text'> As you explore other people's React apps, keep an eye out for common JavaScript patterns. 
+                You should start to recognize some of the concepts you've learned. </div>
+                <a href='./webd' className='box-button'> VIEW ALL </a>
+                </div>
+                </div>
+
             </div>
         )     
     }
