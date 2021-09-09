5598fd94b2addc14fe63fd5f4b8322e1b8b4eb6e
yavnikaa <yavnikagarg3435@gmail.com>
2021-09-09 21:49:41 +0530

    Add structure for about page

--- src/components/about.js
+++ src/components/about.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import '../index.css';
 import '../index.scss';
 import SubHeader from './sub-header';
+import me from '../assets/me.png';
 
 class About extends Component{
     render(){ 
@@ -13,6 +14,12 @@ class About extends Component{
         return (
             <div>
                 <SubHeader headerData={header}/>
+                <div className='about-container'> 
+                <img src={me} max-height="200px"/> 
+                <div className='about-page-text'> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>
+                </div>
+                <div> 
+                    <div className='heading-about'> MEET MY FRIENDS AND MENTORS!</div>
                 <div className="swiper-container">
                     <div className="swiper-wrapper">
                         <div className="swiper-slide">
@@ -30,6 +37,7 @@ class About extends Component{
                             </div>
                         </div>
                         
+                        </div>
                         </div>
                         </div>
                         </div>
